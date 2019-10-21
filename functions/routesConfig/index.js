691cffd0966e9acdb6c086e7e1392416593b65be
Nutthapat Piasangka <my_nutthapat-26_@hotmail.com>
2019-10-21 07:53:37 +0700

    update

--- functions/routesConfig/index.js
+++ functions/routesConfig/index.js
@@ -1,25 +1,43 @@
 const cors = require("cors");
 const {
-    setUser, setGEOLocation, setDateTime,
-    createShareLocation, setOS
+    postUser, postGEOLocation,postStatusProcess,postBaseShareLocation,
+    postShareLocationChat,putShareLocationJoin,getShareLocationPrivate,
+    getShareLocationPublic,postProfile,putProfile,getProfile,getGEOLocation
 } = require('./module')
 
 const corsPostOptions = {
     origin: true,
-    methods: ["POST"],
+    methods: ["POST","GET","PUT","OPTIONS"],
     credentials: true,
     maxAge: 3600
 };
 
 function routesConfig(app) {
     app.options("/user/:id", cors(corsPostOptions));
-    app.options("/geoLocation/:id", cors(corsPostOptions));
+    app.options("/geolocation/:id", cors(corsPostOptions));
+    app.options("/status_process/:id", cors(corsPostOptions));
+    app.options("/base_share_location/:id", cors(corsPostOptions));
+    app.options('/share_location/:hid/join/:uid', cors(corsPostOptions));
+    app.options("/share_location/:hid/chat", cors(corsPostOptions));
+    app.options("/share_location_private/:id", cors(corsPostOptions));
+    app.options("/share_location_public", cors(corsPostOptions));
+    app.options("/profile/:id", cors(corsPostOptions));
 
-    app.post('/user/:id', cors(corsPostOptions), setUser);
-    app.post('/geoLocation/:id', cors(corsPostOptions), setGEOLocation)
-    // app.post('/dateTime/:id', setDateTime)
-    // app.post('/shareLocation/:id', createShareLocation)
-    // app.post('/os/:id', setOS)
+    app.post('/user/:id', cors(corsPostOptions), postUser);
+    app.post('/geolocation/:id', cors(corsPostOptions), postGEOLocation);
+    app.post('/status_process/:id', cors(corsPostOptions), postStatusProcess);
+    app.post('/base_share_location/:id', cors(corsPostOptions), postBaseShareLocation);
+    app.post('/share_location/:hid/chat', cors(corsPostOptions), postShareLocationChat);
+    app.post('/profile/:id', cors(corsPostOptions), postProfile);
+
+    app.put('/share_location/:hid/join/:uid', cors(corsPostOptions), putShareLocationJoin);
+    app.put('/profile/:id', cors(corsPostOptions), putProfile);
+
+    app.get('/share_location_private/:id', cors(corsPostOptions), getShareLocationPrivate);
+    app.get('/share_location_public', cors(corsPostOptions), getShareLocationPublic);
+    app.get('/profile/:id', cors(corsPostOptions), getProfile);
+    app.get('/geolocation/:id', cors(corsPostOptions), getGEOLocation);
+
 }
 
 module.exports = routesConfig;
--- functions/routesConfig/module.js
+++ functions/routesConfig/module.js
@@ -16,13 +16,11 @@ admin.initializeApp({
 
 
 
-exports.setUser = async (req, res) => {
+exports.postUser = async (req, res) => {
 
     try {
 
         await admin.database().ref(`users/${req.params.id}`).set(JSON.parse(req.body))
-        await admin.database().ref(`user_and_location/${req.params.id}/user`).set(JSON.parse(req.body))
-        await admin.database().ref(`user_and_location/${req.params.id}/id`).set(req.params.id)
         // res.status(201).json(req)
         res.send(`บันทึกข้อมูลผู้ใช้ เสร็จสิ้น`);
         // console.log(req.body);
@@ -39,12 +37,10 @@ exports.setUser = async (req, res) => {
 
 };
 
-exports.setGEOLocation = async (req, res) => {
+exports.postGEOLocation = async (req, res) => {
 
     try {
         await admin.database().ref(`geolocation/${req.params.id}`).set(JSON.parse(req.body))
-        await admin.database().ref(`user_and_location/${req.params.id}/location`).set(JSON.parse(req.body))
-        await admin.database().ref(`user_and_location/${req.params.id}/id`).set(req.params.id)
         // res.status(201).json(req)
         res.send(`บันทึกข้อมูล geoLocation เสร็จสิ้น`);
 
@@ -61,11 +57,30 @@ exports.setGEOLocation = async (req, res) => {
     }
 };
 
-exports.setDateTime = async (req, res) => {
+exports.getGEOLocation = async (req, res) => {
 
     try {
 
-        await admin.database().ref(`dateTime/${req.params.id}`).set(JSON.parse(req.body))
+        await admin.database().ref(`geolocation/${req.params.id}`).once("value").then(function (snapshot) {
+            let data = (snapshot.val())
+            return res.send(data.data)
+        })
+
+    } catch (err) {
+
+        res.send(err.message);
+
+        console.log(err.message);
+
+        return res.sendStatus(500)
+    }
+};
+
+exports.postStatusProcess = async (req, res) => {
+
+    try {
+
+        await admin.database().ref(`status/process/${req.params.id}`).set(JSON.parse(req.body))
 
         // res.status(201).json(req)
         // res.send(`บันทึกข้อมูล date time เสร็จสิ้น`);
@@ -82,35 +97,15 @@ exports.setDateTime = async (req, res) => {
     }
 };
 
-exports.setOS = async (req, res) => {
+
+exports.postBaseShareLocation = async (req, res) => {
 
     try {
 
-        await admin.database().ref(`os/${req.params.id}`).set(JSON.parse(req.body))
+        await admin.database().ref(`base_share_location/${req.params.id}`).set(req.body)
 
         // res.status(201).json(req)
-        res.send(`บันทึกข้อมูล os เสร็จสิ้น`);
-
-        console.log(req.body);
-
-    } catch (err) {
-
-        res.send(err.message);
-
-        console.log(err.message);
-
-        return res.sendStatus(500)
-    }
-};
-
-exports.createShareLocation = async (req, res) => {
-
-    try {
-
-        await admin.database().ref(`shareLocation/${req.params.id}`).set(req.body)
-
-        // res.status(201).json(req)
-        res.send(`บันทึกข้อมูล share location เสร็จสิ้น`);
+        res.send(`บันทึกข้อมูล base share location เสร็จสิ้น`);
 
         console.log(res);
 
@@ -123,3 +118,161 @@ exports.createShareLocation = async (req, res) => {
         return res.sendStatus(500)
     }
 };
+
+exports.putShareLocationJoin = async (req, res) => {
+
+    try {
+
+        await admin.database().ref(`share_location/${req.params.hid}/join/${req.params.uid}`).set(JSON.parse(req.body))
+        // res.status(201).json(req)
+        res.send(`อัพเดต share location เสร็จสิ้น`);
+        // console.log(req.body);
+
+
+    } catch (err) {
+
+        res.send(err.message);
+
+        console.log(err.message);
+
+        return res.sendStatus(500)
+    }
+
+};
+
+exports.postShareLocationChat = async (req, res) => {
+
+    try {
+
+        await admin.database().ref(`share_location/${req.params.hid}/chat`).push(JSON.parse(req.body))
+        // res.status(201).json(req)
+        res.send(`อัพเดต share location เสร็จสิ้น`);
+        // console.log(req.body);
+
+
+    } catch (err) {
+
+        res.send(err.message);
+
+        console.log(err.message);
+
+        return res.sendStatus(500)
+    }
+
+};
+
+exports.getShareLocationPrivate = async (req, res) => {
+
+    try {
+
+        await admin.database().ref(`base_share_location/${req.params.id}`).once("value").then(function (snapshot) {
+            let data = (snapshot.val())
+            if (data !== null) {
+                return res.send(data)
+            } else {
+                admin.database().ref(`base_share_location/${req.params.id}`).once("value").then(function (snapshot) {
+                    let user = (snapshot.val())
+                    admin.database().ref(`share_location/${req.params.id}`).set(user)
+                })
+            }
+        })
+
+    } catch (err) {
+
+        res.send(err.message);
+
+        console.log(err.message);
+
+        return res.sendStatus(500)
+    }
+};
+
+exports.getShareLocationPublic = async (req, res) => {
+
+    try {
+
+        await admin.database().ref(`share_location/`).once("value").then(function (snapshot) {
+            let data = (snapshot.val())
+            return res.send(data)
+        })
+
+    } catch (err) {
+
+        res.send(err.message);
+
+        console.log(err.message);
+
+        return res.sendStatus(500)
+    }
+};
+
+exports.postProfile = async (req, res) => {
+
+    try {
+
+        await admin.database().ref(`profile/${req.params.id}`).set(JSON.parse(req.body))
+        // res.status(201).json(req)
+        res.send(`บันทึกข้อมูลผู้ใช้ profile เสร็จสิ้น`);
+        // console.log(req.body);
+
+
+    } catch (err) {
+
+        res.send(err.message);
+
+        console.log(err.message);
+
+        return res.sendStatus(500)
+    }
+
+};
+
+exports.putProfile = async (req, res) => {
+
+    try {
+
+        await admin.database().ref(`profile/${req.params.id}`).update(JSON.parse(req.body))
+        // res.status(201).json(req)
+        res.send(`อัพเดต profile เสร็จสิ้น`);
+        // console.log(req.body);
+
+
+    } catch (err) {
+
+        res.send(err.message);
+
+        console.log(err.message);
+
+        return res.sendStatus(500)
+    }
+
+};
+
+exports.getProfile = async (req, res) => {
+
+    try {
+
+        await admin.database().ref(`profile/${req.params.id}`).once("value").then(function (snapshot) {
+            let data = (snapshot.val())
+            if (data !== null) {
+                return res.send(data)
+            } else {
+                admin.database().ref(`users/${req.params.id}`).once("value").then(function (snapshot) {
+                    let user = (snapshot.val())
+                    admin.database().ref(`profile/${req.params.id}`).set(user.data.providerData[0])
+                })
+            }
+        })
+
+
+    } catch (err) {
+
+        res.send(err.message);
+
+        console.log(err.message);
+
+        return res.sendStatus(500)
+    }
+
+};
+
