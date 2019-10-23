6fe7df7334a36d07805ce84843b7f877939aab73
Nutthapat Piasangka <my_nutthapat-26_@hotmail.com>
2019-10-24 06:53:49 +0700

    update

---
+++ functions/routesConfig/controller.js
@@ -0,0 +1,648 @@
+// const os = require('os')
+// const fs = require('fs');
+// const url = require('url');
+// const functions = require('firebase-functions');
+const { updateObjectDatabase, updateArrayDatabase, updateValueDatabase, getDatabase } = require('./module')
+
+
+function checkError(err, res) {
+    res.send(err.message);
+
+    console.log(err.message);
+
+    return res.sendStatus(500)
+}
+
+exports.post = {
+    users: {
+        user: async (req, res) => {
+            let path = `users/${req.params.id}/user`;
+            let data = JSON.parse(req.body);
+
+            try {
+
+                await updateObjectDatabase(path, data)
+
+                res.send(`บันทึกข้อมูล ${path} เสร็จสิ้น`);
+
+            } catch (error) {
+                return checkError(err, res)
+            }
+        },
+        location: async (req, res) => {
+            let path = `users/${req.params.id}/location`;
+            let data = JSON.parse(req.body);
+
+            try {
+
+                await updateObjectDatabase(path, data)
+
+                res.send(`บันทึกข้อมูล ${path} เสร็จสิ้น`);
+
+            } catch (error) {
+                return checkError(err, res)
+            }
+        },
+        profile: async (req, res) => {
+            let path = `users/${req.params.id}/profile`;
+            let data = JSON.parse(req.body);
+
+            try {
+
+                await updateObjectDatabase(path, data)
+
+                res.send(`บันทึกข้อมูล ${path} เสร็จสิ้น`);
+
+            } catch (error) {
+                return checkError(err, res)
+            }
+        },
+        log: {
+            user: async (req, res) => {
+                let path = `users/${req.params.id}/_log/user`;
+                let data = JSON.parse(req.body);
+
+                try {
+
+                    await updateArrayDatabase(path, data)
+
+                    res.send(`บันทึกข้อมูล ${path} เสร็จสิ้น`);
+
+                } catch (error) {
+                    return checkError(err, res)
+                }
+            },
+            location: async (req, res) => {
+                let path = `users/${req.params.id}/_log/location`;
+                let data = JSON.parse(req.body);
+
+                try {
+
+                    await updateArrayDatabase(path, data)
+
+                    res.send(`บันทึกข้อมูล ${path} เสร็จสิ้น`);
+
+                } catch (error) {
+                    return checkError(err, res)
+                }
+            },
+            profile: async (req, res) => {
+                let path = `users/${req.params.id}/_log/profile`;
+                let data = JSON.parse(req.body);
+
+                try {
+
+                    await updateArrayDatabase(path, data)
+
+                    res.send(`บันทึกข้อมูล ${path} เสร็จสิ้น`);
+
+                } catch (error) {
+                    return checkError(err, res)
+                }
+            }
+        }
+    },
+    share: {
+        location: async (req, res) => {
+            let path = `share/${req.params.id}/location`;
+            let data = JSON.parse(req.body);
+
+            try {
+
+                await updateObjectDatabase(path, data)
+
+                res.send(`บันทึกข้อมูล ${path} เสร็จสิ้น`);
+
+            } catch (error) {
+                return checkError(err, res)
+            }
+        },
+        date: async (req, res) => {
+            let path = `share/${req.params.id}/date`;
+            let data = JSON.parse(req.body);
+
+            try {
+
+                await updateObjectDatabase(path, data)
+
+                res.send(`บันทึกข้อมูล ${path} เสร็จสิ้น`);
+
+            } catch (error) {
+                return checkError(err, res)
+            }
+        },
+        max_number: async (req, res) => {
+            let path = `share/${req.params.id}/max_number`;
+            let data = JSON.parse(req.body);
+
+            try {
+
+                await updateValueDatabase(path, data)
+
+                res.send(`บันทึกข้อมูล ${path} เสร็จสิ้น`);
+
+            } catch (error) {
+                return checkError(err, res)
+            }
+        },
+        sex: async (req, res) => {
+            let path = `share/${req.params.id}/sex`;
+            let data = JSON.parse(req.body);
+
+            try {
+
+                await updateValueDatabase(path, data)
+
+                res.send(`บันทึกข้อมูล ${path} เสร็จสิ้น`);
+
+            } catch (error) {
+                return checkError(err, res)
+            }
+        },
+        owner: async (req, res) => {
+            let path = `share/${req.params.id}/owner`;
+            let data = JSON.parse(req.body);
+
+            try {
+
+                await updateValueDatabase(path, data)
+
+                res.send(`บันทึกข้อมูล ${path} เสร็จสิ้น`);
+
+            } catch (error) {
+                return checkError(err, res)
+            }
+        },
+        member: async (req, res) => {
+            let path = `share/${req.params.id}/member`;
+            let data = JSON.parse(req.body);
+
+            try {
+
+                await updateObjectDatabase(path, data)
+
+                res.send(`บันทึกข้อมูล ${path} เสร็จสิ้น`);
+
+            } catch (error) {
+                return checkError(err, res)
+            }
+        },
+        chat: async (req, res) => {
+            let path = `share/${req.params.id}/chat`;
+            let data = JSON.parse(req.body);
+
+            try {
+
+                await updateObjectDatabase(path, data)
+
+                res.send(`บันทึกข้อมูล ${path} เสร็จสิ้น`);
+
+            } catch (error) {
+                return checkError(err, res)
+            }
+        },
+        log: {
+            location: async (req, res) => {
+                let path = `share/${req.params.id}/_log/location`;
+                let data = JSON.parse(req.body);
+
+                try {
+
+                    await updateArrayDatabase(path, data)
+
+                    res.send(`บันทึกข้อมูล ${path} เสร็จสิ้น`);
+
+                } catch (error) {
+                    return checkError(err, res)
+                }
+            },
+            date: async (req, res) => {
+                let path = `share/${req.params.id}/_log/date`;
+                let data = JSON.parse(req.body);
+
+                try {
+
+                    await updateArrayDatabase(path, data)
+
+                    res.send(`บันทึกข้อมูล ${path} เสร็จสิ้น`);
+
+                } catch (error) {
+                    return checkError(err, res)
+                }
+            },
+            max_number: async (req, res) => {
+                let path = `share/${req.params.id}/_log/max_number`;
+                let data = JSON.parse(req.body);
+
+                try {
+
+                    await updateArrayDatabase(path, data)
+
+                    res.send(`บันทึกข้อมูล ${path} เสร็จสิ้น`);
+
+                } catch (error) {
+                    return checkError(err, res)
+                }
+            },
+            sex: async (req, res) => {
+                let path = `share/${req.params.id}/_log/sex`;
+                let data = JSON.parse(req.body);
+
+                try {
+
+                    await updateArrayDatabase(path, data)
+
+                    res.send(`บันทึกข้อมูล ${path} เสร็จสิ้น`);
+
+                } catch (error) {
+                    return checkError(err, res)
+                }
+            },
+            owner: async (req, res) => {
+                let path = `share/${req.params.id}/_log/owner`;
+                let data = JSON.parse(req.body);
+
+                try {
+
+                    await updateArrayDatabase(path, data)
+
+                    res.send(`บันทึกข้อมูล ${path} เสร็จสิ้น`);
+
+                } catch (error) {
+                    return checkError(err, res)
+                }
+            },
+            member: async (req, res) => {
+                let path = `share/${req.params.id}/_log/member`;
+                let data = JSON.parse(req.body);
+
+                try {
+
+                    await updateArrayDatabase(path, data)
+
+                    res.send(`บันทึกข้อมูล ${path} เสร็จสิ้น`);
+
+                } catch (error) {
+                    return checkError(err, res)
+                }
+            },
+            chat: async (req, res) => {
+                let path = `share/${req.params.id}/_log/chat`;
+                let data = JSON.parse(req.body);
+
+                try {
+
+                    await updateArrayDatabase(path, data)
+
+                    res.send(`บันทึกข้อมูล ${path} เสร็จสิ้น`);
+
+                } catch (error) {
+                    return checkError(err, res)
+                }
+            },
+        }
+    }
+}
+
+exports.get = {
+    users: {
+        location: async (req, res) => {
+            let path = `users/${req.params.id}/location`;
+
+            try {
+
+                await getDatabase(path).then(function (snapshot) {
+                    let data = (snapshot.val())
+                    return res.send(data)
+
+                })
+
+            } catch (err) {
+                return checkError(err, res)
+            }
+        },
+        profile: async (req, res) => {
+            let path_profile = `users/${req.params.id}/profile`;
+            let path_user = `users/${req.params.id}/user`;
+
+            try {
+
+                await getDatabase(path_profile).then(function (snapshot) {
+                    let data = (snapshot.val())
+                    if (data !== null) {
+                        return res.send(data)
+                    } else {
+                        getDatabase(path_user).then(function (snapshot) {
+                            let user = (snapshot.val())
+                            console.log(user);
+
+                            updateObjectDatabase(path_profile, user.providerData[0])
+                        })
+                    }
+                })
+
+            } catch (err) {
+                return checkError(err, res)
+            }
+        }
+    }
+}
+
+// exports.postUser = async (req, res) => {
+
+//     try {
+
+//         await admin.database().ref(`users/${req.params.id}`).set(JSON.parse(req.body))
+//         // res.status(201).json(req)
+//         res.send(`บันทึกข้อมูลผู้ใช้ เสร็จสิ้น`);
+//         // console.log(req.body);
+
+
+//     } catch (err) {
+
+//         return checkError(err, res)
+//     }
+
+// };
+
+// exports.postGEOLocation = async (req, res) => {
+
+//     try {
+//         await admin.database().ref(`geolocation/${req.params.id}`).set(JSON.parse(req.body))
+//         // res.status(201).json(req)
+//         res.send(`บันทึกข้อมูล geoLocation เสร็จสิ้น`);
+
+//         // console.log(req.body);
+
+
+//     } catch (err) {
+
+//         res.send(err.message);
+
+//         console.log(err.message);
+
+//         return res.sendStatus(500)
+//     }
+// };
+
+// exports.getGEOLocation = async (req, res) => {
+
+//     try {
+
+//         await admin.database().ref(`geolocation/${req.params.id}`).once("value").then(function (snapshot) {
+//             let data = (snapshot.val())
+//             return res.send(data.data)
+//         })
+
+//     } catch (err) {
+
+//         res.send(err.message);
+
+//         console.log(err.message);
+
+//         return res.sendStatus(500)
+//     }
+// };
+
+// exports.postStatusProcess = async (req, res) => {
+
+//     try {
+
+//         await admin.database().ref(`status/${req.params.id}/process`).set(JSON.parse(req.body))
+
+//         // res.status(201).json(req)
+//         // res.send(`บันทึกข้อมูล date time เสร็จสิ้น`);
+
+//         // console.log(res);
+
+//     } catch (err) {
+
+//         res.send(err.message);
+
+//         console.log(err.message);
+
+//         return res.sendStatus(500)
+//     }
+// };
+
+
+// exports.postBaseShareLocation = async (req, res) => {
+
+//     try {
+
+//         await admin.database().ref(`base_share_location/${req.params.id}`).update(JSON.parse(req.body))
+
+//         // res.status(201).json(req)
+//         res.send(`บันทึกข้อมูล base share location เสร็จสิ้น`);
+
+//         // console.log(res);
+
+//     } catch (err) {
+
+//         res.send(err.message);
+
+//         console.log(err.message);
+
+//         return res.sendStatus(500)
+//     }
+// };
+
+// exports.putShareLocationJoin = async (req, res) => {
+
+//     try {
+
+//         await admin.database().ref(`share_location/${req.params.hid}/join/${req.params.uid}`).set(JSON.parse(req.body))
+//         // res.status(201).json(req)
+//         res.send(`อัพเดต share location เสร็จสิ้น`);
+//         // console.log(req.body);
+
+
+//     } catch (err) {
+
+//         res.send(err.message);
+
+//         console.log(err.message);
+
+//         return res.sendStatus(500)
+//     }
+
+// };
+
+// exports.postShareLocationChat = async (req, res) => {
+
+//     try {
+
+//         await admin.database().ref(`share_location/${req.params.hid}/chat`).push(JSON.parse(req.body))
+//         // res.status(201).json(req)
+//         res.send(`อัพเดต share location เสร็จสิ้น`);
+//         // console.log(req.body);
+
+
+//     } catch (err) {
+
+//         res.send(err.message);
+
+//         console.log(err.message);
+
+//         return res.sendStatus(500)
+//     }
+
+// };
+
+// exports.getShareLocationPrivate = async (req, res) => {
+
+//     try {
+
+//         await admin.database().ref(`base_share_location/${req.params.id}`).once("value").then(function (snapshot) {
+//             let data = (snapshot.val())
+//             if (data !== null) {
+//                 return res.send(data)
+//             } else {
+//                 admin.database().ref(`base_share_location/${req.params.id}`).once("value").then(function (snapshot) {
+//                     let user = (snapshot.val())
+//                     admin.database().ref(`share_location/${req.params.id}`).set(user)
+//                 })
+//             }
+//         })
+
+//     } catch (err) {
+
+//         res.send(err.message);
+
+//         console.log(err.message);
+
+//         return res.sendStatus(500)
+//     }
+// };
+
+// exports.getShareLocationPublic = async (req, res) => {
+
+//     try {
+
+//         await admin.database().ref(`share_location/`).once("value").then(function (snapshot) {
+//             let data = (snapshot.val())
+//             return res.send(data)
+//         })
+
+//     } catch (err) {
+
+//         res.send(err.message);
+
+//         console.log(err.message);
+
+//         return res.sendStatus(500)
+//     }
+// };
+
+// exports.postProfile = async (req, res) => {
+//     // console.log(JSON.parse(req.body));
+
+//     try {
+
+//         await admin.database().ref(`profile/${req.params.id}`).update(JSON.parse(req.body))
+//         // res.status(201).json(req)
+//         res.send(`บันทึกข้อมูลผู้ใช้ profile เสร็จสิ้น`);
+//         // console.log(req.body);
+
+
+//     } catch (err) {
+
+//         res.send(err.message);
+
+//         console.log(err.message);
+
+//         return res.sendStatus(500)
+//     }
+
+// };
+
+// exports.putProfile = async (req, res) => {
+
+//     try {
+
+//         await admin.database().ref(`profile/${req.params.id}`).update(JSON.parse(req.body))
+//         // res.status(201).json(req)
+//         res.send(`อัพเดต profile เสร็จสิ้น`);
+//         // console.log(req.body);
+
+
+//     } catch (err) {
+
+//         res.send(err.message);
+
+//         console.log(err.message);
+
+//         return res.sendStatus(500)
+//     }
+
+// };
+
+// exports.getProfile = async (req, res) => {
+
+//     try {
+
+//         await admin.database().ref(`profile/${req.params.id}`).once("value").then(function (snapshot) {
+//             let data = (snapshot.val())
+//             if (data !== null) {
+//                 return res.send(data)
+//             } else {
+//                 admin.database().ref(`users/${req.params.id}`).once("value").then(function (snapshot) {
+//                     let user = (snapshot.val())
+//                     admin.database().ref(`profile/${req.params.id}`).set(user.data.providerData[0])
+//                 })
+//             }
+//         })
+
+
+//     } catch (err) {
+
+//         res.send(err.message);
+
+//         console.log(err.message);
+
+//         return res.sendStatus(500)
+//     }
+
+// };
+
+// exports.postStatusShare = async (req, res) => {
+
+//     try {
+
+//         await admin.database().ref(`status/${req.params.share_id}/share`).set(JSON.parse(req.body))
+//         // res.status(201).json(req)
+//         res.send(`อัพเดต status share เสร็จสิ้น`);
+//         // console.log(req.body);
+//     } catch (err) {
+
+//         res.send(err.message);
+
+//         console.log(err.message);
+
+//         return res.sendStatus(500)
+//     }
+// };
+
+// exports.getStatusShare = async (req, res) => {
+
+//     try {
+
+//         await admin.database().ref(`status/${req.params.share_id}`).once("value").then(function (snapshot) {
+//             let data = (snapshot.val())
+//             if (data !== null) {
+//                 return res.send(data)
+//             } else {
+//                 return res.send({ share: false })
+//             }
+//         })
+
+//     } catch (err) {
+
+//         res.send(err.message);
+
+//         console.log(err.message);
+
+//         return res.sendStatus(500)
+//     }
+
+// };
--- functions/routesConfig/index.js
+++ functions/routesConfig/index.js
@@ -1,46 +1,76 @@
 const cors = require("cors");
-const {
-    postUser, postGEOLocation,postStatusProcess,postBaseShareLocation,
-    postShareLocationChat,putShareLocationJoin,getShareLocationPrivate,
-    getShareLocationPublic,postProfile,putProfile,getProfile,getGEOLocation,
-    getStatusShare,postStatusShare
-} = require('./module')
+const { post, get } = require('./controller')
 
-const corsPostOptions = {
+const corsOptions = {
     origin: true,
-    methods: ["POST","GET","PUT","OPTIONS"],
+    methods: ["POST", "GET", "PUT", "OPTIONS"],
     credentials: true,
     maxAge: 3600
 };
 
 function routesConfig(app) {
-    app.options("/user/:id", cors(corsPostOptions));
-    app.options("/geolocation/:id", cors(corsPostOptions));
-    app.options("/status_process/:id", cors(corsPostOptions));
-    app.options("/base_share_location/:id", cors(corsPostOptions));
-    app.options('/share_location/:hid/join/:uid', cors(corsPostOptions));
-    app.options("/share_location/:hid/chat", cors(corsPostOptions));
-    app.options("/share_location_private/:id", cors(corsPostOptions));
-    app.options("/share_location_public", cors(corsPostOptions));
-    app.options("/profile/:id", cors(corsPostOptions));
-    app.options('/status_share/:share_id', cors(corsPostOptions));
+    app.options("/users/:id/user", cors(corsOptions));
+    app.options("/users/:id/location", cors(corsOptions));
+    app.options("/users/:id/profile", cors(corsOptions));
+    app.options("/users/:id/_log/user", cors(corsOptions));
+    app.options("/users/:id/_log/location", cors(corsOptions));
+    app.options("/users/:id/_log/profile", cors(corsOptions));
+    app.options("/share/:id/location", cors(corsOptions));
+    app.options("/share/:id/date", cors(corsOptions));
+    app.options("/share/:id/max_number", cors(corsOptions));
+    app.options("/share/:id/sex", cors(corsOptions));
+    app.options("/share/:id/owner", cors(corsOptions));
+    app.options("/share/:id/member", cors(corsOptions));
+    app.options("/share/:id/chat", cors(corsOptions));
+    app.options("/share/:id/_log/location", cors(corsOptions));
+    app.options("/share/:id/_log/date", cors(corsOptions));
+    app.options("/share/:id/_log/max_number", cors(corsOptions));
+    app.options("/share/:id/_log/sex", cors(corsOptions));
+    app.options("/share/:id/_log/owner", cors(corsOptions));
+    app.options("/share/:id/_log/member", cors(corsOptions));
+    app.options("/share/:id/_log/chat", cors(corsOptions));
+    app.options("/status/:id/process", cors(corsOptions));
+    app.options("/status/:id/share", cors(corsOptions));
+    app.options("/status/:id/share", cors(corsOptions));
+    app.options("/status/:id/owner", cors(corsOptions));
+    app.options("/status/:id/member", cors(corsOptions));
+    app.options("/status/:id/alert", cors(corsOptions));
+    app.options("/status/:id/_log", cors(corsOptions));
 
-    app.post('/user/:id', cors(corsPostOptions), postUser);
-    app.post('/geolocation/:id', cors(corsPostOptions), postGEOLocation);
-    app.post('/status_process/:id', cors(corsPostOptions), postStatusProcess);
-    app.post('/base_share_location/:id', cors(corsPostOptions), postBaseShareLocation);
-    app.post('/share_location/:hid/chat', cors(corsPostOptions), postShareLocationChat);
-    app.post('/profile/:id', cors(corsPostOptions), postProfile);
-    app.post('/status_share/:share_id', cors(corsPostOptions), postStatusShare);
 
-    app.put('/share_location/:hid/join/:uid', cors(corsPostOptions), putShareLocationJoin);
-    app.put('/profile/:id', cors(corsPostOptions), putProfile);
+    app.post("/users/:id/user", cors(corsOptions), post.users.user);
+    app.post("/users/:id/location", cors(corsOptions), post.users.location);
+    app.post("/users/:id/profile", cors(corsOptions), post.users.profile);
+    app.post("/users/:id/_log/user", cors(corsOptions), post.users.log.user);
+    app.post("/users/:id/_log/location", cors(corsOptions), post.users.log.location);
+    app.post("/users/:id/_log/profile", cors(corsOptions), post.users.log.profile);
+    app.post("/share/:id/location", cors(corsOptions), post.share.location);
+    app.post("/share/:id/date", cors(corsOptions), post.share.date);
+    app.post("/share/:id/max_number", cors(corsOptions), post.share.max_number);
+    app.post("/share/:id/sex", cors(corsOptions), post.share.sex);
+    app.post("/share/:id/owner", cors(corsOptions), post.share.owner);
+    app.post("/share/:id/member", cors(corsOptions), post.share.member);
+    app.post("/share/:id/chat", cors(corsOptions), post.share.chat);
+    app.post("/share/:id/_log/location", cors(corsOptions), post.share.log.location);
+    app.post("/share/:id/_log/date", cors(corsOptions), post.share.log.date);
+    app.post("/share/:id/_log/max_number", cors(corsOptions), post.share.log.max_number);
+    app.post("/share/:id/_log/sex", cors(corsOptions), post.share.log.sex);
+    app.post("/share/:id/_log/owner", cors(corsOptions), post.share.log.owner);
+    app.post("/share/:id/_log/member", cors(corsOptions), post.share.log.member);
+    app.post("/share/:id/_log/chat", cors(corsOptions), post.share.log.chat);
+    app.post("/status/:id/process", cors(corsOptions));
+    app.post("/status/:id/share", cors(corsOptions));
+    app.post("/status/:id/share", cors(corsOptions));
+    app.post("/status/:id/owner", cors(corsOptions));
+    app.post("/status/:id/member", cors(corsOptions));
+    app.post("/status/:id/alert", cors(corsOptions));
+    app.post("/status/:id/_log", cors(corsOptions));
 
-    app.get('/share_location_private/:id', cors(corsPostOptions), getShareLocationPrivate);
-    app.get('/share_location_public', cors(corsPostOptions), getShareLocationPublic);
-    app.get('/profile/:id', cors(corsPostOptions), getProfile);
-    app.get('/geolocation/:id', cors(corsPostOptions), getGEOLocation);
-    app.get('/status_share/:share_id', cors(corsPostOptions), getStatusShare);
+    app.get("/users/:id", cors(corsOptions));
+    app.get("/users/:id/location", cors(corsOptions), get.users.location);
+    app.get("/users/:id/profile", cors(corsOptions), get.users.profile);
+    app.get("/share/:id", cors(corsOptions));
+    app.get("/status/:id", cors(corsOptions));
 
 }
 
